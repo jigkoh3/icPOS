1041fabaffc5f9e1b85f65a9c83590a59b104643
jigkoh3 <jigkoh3@hotmail.com>
2018-01-21 17:35:51 +0700

    add order step

--- src/assets/models/product.model.ts
+++ src/assets/models/product.model.ts
@@ -1,5 +1,29 @@
-export class ProductModel{
+export class ProductModel {
     name: String;
     image: String;
     bgcoler: String;
+    category: CategoryModel;
+    prices: Array<PriceModel>;
+    submenus: Array<SubmenuModel>
+    description: String;
+}
+
+export class CategoryModel {
+    name: String;
+}
+
+export class PriceModel {
+    name: String;
+    type: String;
+    price: Number;
+}
+export class SubmenuModel {
+    name: String;
+    prices: Array<PriceModel>;
+    isradio: Boolean;
+    bom: BomModel;
+}
+
+export class BomModel{
+    name: String;
 }
--- src/pages/product-order/product-order.ts
+++ src/pages/product-order/product-order.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ProductModel } from '../../assets/models/product.model';
 
 /**
  * Generated class for the ProductOrderPage page.
@@ -14,11 +15,33 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'product-order.html',
 })
 export class ProductOrderPage {
-  item: any;
+  item: ProductModel;
   step: number = 0;
+  steps = [];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.item = this.navParams.get('item');
-    console.log(this.item);
+    this.steps = this.calculatestep();
+    console.log(this.steps);
+  }
+
+  calculatestep():Array<any> {
+    let steps = [];
+    if (this.item.prices.length > 1) {
+       steps.push({
+        name: "price",
+        type: "price"
+      })
+
+    }
+    this.item.submenus.forEach(function (sub) {
+      steps.push({
+        name: sub.name,
+        type: "submenu",
+        data: sub
+      })
+    });
+
+    return steps;
   }
 
   next() {
@@ -26,9 +49,13 @@ export class ProductOrderPage {
   }
 
   prev() {
-    if(this.step > 0){
+    if (this.step > 0) {
       this.step -= 1;
     }
+  }
+
+  add(){
+    alert("Finish");
   }
 
 }
