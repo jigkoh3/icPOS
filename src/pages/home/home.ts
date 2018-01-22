43b3961ccdd6a73697eab4a11436457a265bd124
jigkoh3 <jigkoh3@hotmail.com>
2018-01-22 14:37:17 +0700

    add to order finish#1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,6 +8,7 @@ import { ProductOrderComponent } from '../../components/product-order/product-or
 import { ProductOrderPage } from '../product-order/product-order';
 import { OrderProvider } from '../../providers/order/order';
 import { OrderModel, OrderItemModel } from '../../assets/models/order.model';
+import { ProductModel } from '../../assets/models/product.model';
 
 @Component({
   selector: 'page-home',
@@ -30,7 +31,7 @@ export class HomePage {
   }
 
   ionViewDidLoad() {
-    this.getOrderData();
+    //this.getOrderData();
     this.getMenuData();
   }
 
@@ -87,6 +88,13 @@ export class HomePage {
         if(item.product.prices && item.product.submenus){
           if (item.product.prices.length > 1 || item.product.submenus.length > 0) {
             this.presentProductModal(item.product);
+          }else{
+            let _item = new OrderItemModel();
+            _item.product = item.product;
+            _item.type = "product";
+            _item.qty = 1;
+            _item.total = item.product.prices[0].price;
+            this.updateOrder(_item);
           }
         }
         
@@ -99,8 +107,22 @@ export class HomePage {
     }
   }
 
+  updateOrder(item){
+    if(this.order){
+      this.order.push(item);
+    }else{
+      this.order = [];
+      this.order.push(item);
+    }
+    
+  }
+
   presentProductModal(item) {
     let productModal = this.modalCtrl.create(ProductOrderPage, { item: item });
+    productModal.onDidDismiss(data => {
+      console.log(data);
+      this.updateOrder(data);
+    });
     productModal.present();
   }
 
--- src/pages/product-order/product-order.ts
+++ src/pages/product-order/product-order.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { ProductModel } from '../../assets/models/product.model';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import { ProductModel, SubmenuModel } from '../../assets/models/product.model';
+import { OrderItemModel } from '../../assets/models/order.model';
 
 /**
  * Generated class for the ProductOrderPage page.
@@ -18,10 +19,17 @@ export class ProductOrderPage {
   item: ProductModel;
   step: number = 0;
   steps = [];
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  result:OrderItemModel;
+  constructor(public viewCtrl: ViewController, public navParams: NavParams) {
     this.item = this.navParams.get('item');
     this.steps = this.calculatestep();
-    //console.log(this.steps);
+    this.result = new OrderItemModel();
+    this.result.product = this.item;
+    this.result.product.prices = [];
+    this.result.product.submenus =[];
+    this.result.total = 0;
+    this.result.qty = 1;
+    console.log(this.result);
   }
 
   calculatestep():Array<any> {
@@ -30,7 +38,8 @@ export class ProductOrderPage {
        steps.push({
         name: "price",
         type: "price",
-        data: this.item.prices
+        data: this.item.prices,
+        result:null
       })
 
     }
@@ -38,7 +47,8 @@ export class ProductOrderPage {
       steps.push({
         name: sub.name,
         type: "submenu",
-        data: sub
+        data: sub,
+        result: null
       })
     });
 
@@ -46,17 +56,71 @@ export class ProductOrderPage {
   }
 
   next() {
+    //console.log(this.result);
+    this.updateOrderItem(this.step)
     this.step += 1;
+    //console.log(this.steps);
+    
   }
 
   prev() {
     if (this.step > 0) {
       this.step -= 1;
     }
+    //console.log(this.steps);
   }
 
   add(){
-    alert("Finish");
+    this.updateOrderItem(this.step)
+    this.viewCtrl.dismiss(this.result);
+  }
+
+  updateOrderItem(step){
+    let _step = this.steps[step];
+    let _result = this.result;
+
+    switch (_step.type) {
+      case "price": {
+        //statements; 
+        _result.product.prices = [];
+        _step.data.forEach(function(_price){
+          if(_price.name === _step.result){
+            _result.product.prices.push(_price);
+            _result.total += _price.price;
+          }
+        });
+        
+        break;
+      }
+      case "submenu": {
+        //statements;
+        let _submenu = new SubmenuModel();
+        _submenu.name = _step.data.name;
+        _submenu.prices = [];
+
+        _step.data.prices.forEach(function(_price){
+          if(_price.name === _step.result){
+            _submenu.prices.push(_price);
+            _result.total += _price.price;
+          }
+        });
+        let i=0;
+        _result.product.submenus.forEach(function(sub){
+          if(sub.name === _step.name){
+            _result.product.submenus.splice(i);
+          }
+          i++;
+        });
+        _result.product.submenus.push(_submenu);
+        break;
+      }
+      default: {
+        //statements; 
+        break;
+      }
+    }
+    this.result = _result;
+    //console.log(this.result);
   }
 
 }
